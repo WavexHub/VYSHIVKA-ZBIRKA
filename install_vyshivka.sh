ed6200d1600b150212f66d8fd28e8774cde8e59a
WavexHub <163517139+WavexHub@users.noreply.github.com>
2026-02-21 06:49:28 +0000

    Add locale installation and configuration steps
    Install and configure Ukrainian locales in Vyshivka setup.

--- install_vyshivka.sh
+++ install_vyshivka.sh
@@ -1,5 +1,14 @@
 #!/bin/bash 
 
+sudo xbps-install -uy xbps
+sudo xbps-install -Syu
+
+sudo xbps-install -y glibc-locales
+
+sudo sed -i 's/#uk_UA.UTF-8 UTF-8/uk_UA.UTF-8 UTF-8/' /etc/default/libc-locales
+sudo xbps-reconfigure -f glibc-locales
+
+echo "LANG=uk_UA.UTF-8" | sudo tee /etc/locale.conf
 echo "Ласкаво прошу до збірки Vyshivka. Слава Україні!"
 
 sudo xbps-install -S && sudo xbps-pkgdb -a 
