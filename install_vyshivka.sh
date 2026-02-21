06f1f2d0e572bdd1f890bc280f474932be974f99
WavexHub <163517139+WavexHub@users.noreply.github.com>
2026-02-21 07:16:21 +0000

    Update install_vyshivka.sh for XFCE setup
    Added locale settings and installed XFCE desktop environment.

--- install_vyshivka.sh
+++ install_vyshivka.sh
@@ -1,4 +1,6 @@
-#!/bin/bash 
+#!/bin/bash
+
+echo "Pidgotovka!"
 
 git pull origin main
 sudo xbps-install -uy xbps
@@ -9,7 +11,10 @@ sudo xbps-install -y glibc-locales
 sudo sed -i 's/#uk_UA.UTF-8 UTF-8/uk_UA.UTF-8 UTF-8/' /etc/default/libc-locales
 sudo xbps-reconfigure -f glibc-locales
 
-echo "LANG=uk_UA.UTF-8" | sudo tee /etc/locale.conf
+
+echo "LANG=en_US.UTF-8" | sudo tee /etc/locale.conf
+echo "LC_TIME=uk_UA.UTF-8" | sudo tee -a /etc/locale.conf
+
 
 echo "-------------------------------------------------------"
 echo "Ласкаво прошу до збірки Vyshivka. Слава Україні!"
@@ -17,3 +22,15 @@ echo "-------------------------------------------------------"
 
 
 sudo xbps-install -S && sudo xbps-pkgdb -a 
+
+sudo xbps-install -y xfce4 xfce4-plugins lightdm lightdm-gtk-greeter xorg-server xorg-video-drivers
+
+# Менеджер входу (екран вітання)
+sudo ln -s /etc/sv/lightdm /var/service/
+# Мережа (щоб інтернет був відразу)
+sudo ln -s /etc/sv/NetworkManager /var/service/
+# Звук (Pipewire для стабільності)
+sudo xbps-install -y pipewire pavucontrol
+
+sudo mkdir -p /etc/skel/.config/xfce4
+sudo cp -r configs/xfce4/* /etc/skel/.config/xfce4/
